17421cb59ec6e0cc03f5d2226ead7cc8c8755490
GLAUCO WELLISSON CASTRO DE MENDONCA <glauco.mendonca@unicuritiba.com.br>
2023-04-08 20:58:51 -0300

    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        double salariohora;
+        double horatrabalhada;
+        double salariomensal;
+
+        try (Scanner teclado = new Scanner(System.in)) {
+            System.out.println("Quanto você ganha por hora?");
+
+            salariohora = teclado.nextDouble();
+
+            System.out.println("Quantas horas você trabalha no mês?");
+
+            horatrabalhada = teclado.nextDouble();
+
+            salariomensal = salariohora * horatrabalhada;
+
+            System.out.printf("Seu sálario mensal é de:%.1f", salariomensal);
+
+        }
+
+    }
+
+}
